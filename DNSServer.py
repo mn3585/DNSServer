08492f0cad5f7f46dd578133daa207e55a6a43a4
mn3585 <158656925+mn3585@users.noreply.github.com>
2024-03-29 17:53:58 -0400

    DNSServer.py

--- DNSServer.py
+++ DNSServer.py
@@ -90,7 +90,7 @@ dns_records = {
     },
     'nyu.edu.': {
         dns.rdatatype.A: '192.168.1.106',
-        dns.rdatatype.TXT: ('a string cast version of your encrypted secret data from step 3',),
+        dns.rdatatype.TXT: ('a string cast version of your encrypted secret data from step 3'),
         dns.rdatatype.MX: [(10, 'mxa-00256a01.gslb.pphosted.com.')],
         dns.rdatatype.AAAA: '2001:0db8:85a3:0000:0000:8a2e:0373:7312',
         dns.rdatatype.NS: 'ns1.nyu.edu.',   
