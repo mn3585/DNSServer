4242177d6e29ac990d7a70bc3b4698d71c41e096
mn3585 <158656925+mn3585@users.noreply.github.com>
2024-03-30 12:49:52 -0400

    DNSServer.py

--- DNSServer.py
+++ DNSServer.py
@@ -40,7 +40,7 @@ def encrypt_with_aes(input_string, password, salt):
 def decrypt_with_aes(encrypted_data, password, salt):
     key = generate_aes_key(password, salt)
     f = Fernet(key)
-    decrypted_data = str(f.decrypt(encrypted_data.encode('utf-8')))  #call the Fernet encrypt method
+    decrypted_data = f.str(encrypted_data))  #call the Fernet encrypt method
     return decrypted_data
 
 salt = b'Tandon' # Remember it should be a byte-object
