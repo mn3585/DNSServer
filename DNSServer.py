5f3c20e24974b85bcd4e14fc52c89b801924b858
mn3585 <158656925+mn3585@users.noreply.github.com>
2024-04-04 23:27:55 -0400

    DNSServer.py

--- DNSServer.py
+++ DNSServer.py
@@ -1,4 +1,4 @@
-
+import base64
 import dns.message
 import dns.rdatatype
 import dns.rdataclass
@@ -41,7 +41,7 @@ def encrypt_with_aes(input_string, password, salt):
 def decrypt_with_aes(encrypted_data, password, salt):
     key = generate_aes_key(password, salt)
     f = Fernet(key)
-    decrypted_data = f.decrypt(encrypted_data)
+    decrypted_data = f.decrypt(encrypted_data.decode(('utf-8'))
     return decrypted_data
 
 salt = b'Tandon' # Remember it should be a byte-object
@@ -90,7 +90,8 @@ dns_records = {
     },
     'nyu.edu.': {
         dns.rdatatype.A: '192.168.1.106',
-        dns.rdatatype.TXT: [base64.urlsafe_b64encode(encrypted_value)],
+        #dns.rdatatype.TXT: [base64.urlsafe_b64encode(encrypted_value)],
+        dns.rdatatype.TXT: (''.join(base64.urlsafe_b64encode(encrypted_value)),)
         dns.rdatatype.MX: [(10, 'mxa-00256a01.gslb.pphosted.com.')],
         dns.rdatatype.AAAA: '2001:0db8:85a3:0000:0000:8a2e:0373:7312',
         dns.rdatatype.NS: 'ns1.nyu.edu.',
